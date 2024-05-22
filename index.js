4f6bce91466bf5980429050acbe3ae56394fffea
lllHunterlllSV <ia.edwin_hernandez@ufg.edu.sv>
2024-05-22 13:53:57 -0600

    Finished

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('botonCalcular').addEventListener('click', function() {
+    var totalMonto = parseFloat(document.getElementById('totalMonto').value);
+    var capital = parseFloat(document.getElementById('capital').value);
+    var alerta = document.getElementById('alerta');
+
+    if (isNaN(totalMonto) || isNaN(capital)) {
+        alerta.classList.remove('d-none');
+        return;
+    } else {
+        alerta.classList.add('d-none');
+    }
+
+    var interesAdeudado = totalMonto - capital;
+    document.getElementById('resultado').value = interesAdeudado.toFixed(2);
+});
